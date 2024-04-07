4aaa4997c23a057a1caf5a38f9cd1279bc1750be
@VirusLabItalia <145903078+VirusLabIT@users.noreply.github.com>
2024-04-07 18:32:43 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+//questo è un commento
+console.log("helloworld")
+
+document.getElementById("btn1").addEventListener("click", redirectyt);
+function redirectyt(){ window.location = "https://www.youtube.com/channel/UCDzaWsMWNza7oH6Yc2Rpv-w"; }
+
+document.getElementById("btn2").addEventListener("click", redirecttt);
+function redirecttt(){ window.location = "https://www.tiktok.com/@viruslabit"; }
+
+document.getElementById("danipro").addEventListener("click", redirectdani);
+function redirectdani(){ window.location = "https://www.youtube.com/@Daniprogamer01"; }
+
+document.getElementById("twg").addEventListener("click", redirecttwg);
+function redirecttwg(){ window.location = "https://www.youtube.com/@TheWolfGamer-zl2cx"; }
+
+document.getElementById("chb").addEventListener("click", redirectchb);
+function redirectchb(){ window.location = "https://www.youtube.com/@chessbottom"; }
+
+document.getElementById("vivpe").addEventListener("click", redirectviv);
+function redirectviv(){ window.location = "https://www.tiktok.com/@vivpe"; }
